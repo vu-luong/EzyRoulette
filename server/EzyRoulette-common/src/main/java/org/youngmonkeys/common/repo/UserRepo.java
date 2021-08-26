c608d47bbe2090d0c35a5c3f54473f32d26f1a62
Vu Luong <vu.luong@griffithuni.edu.au>
2021-08-26 23:58:38 +1000

    EzyRoulette 005: Set up database on ezyfox server (#4)

---
+++ server/EzyRoulette-common/src/main/java/org/youngmonkeys/common/repo/UserRepo.java
@@ -0,0 +1,7 @@
+package org.youngmonkeys.common.repo;
+
+import com.tvd12.ezyfox.database.annotation.EzyRepository;
+
+@EzyRepository("userRepo")
+public interface UserRepo {
+}
---
+++ server/EzyRoulette-common/src/main/java/org/youngmonkeys/common/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package org.youngmonkeys.common.service.impl;
+
+import com.tvd12.ezyfox.bean.annotation.EzySingleton;
+import org.youngmonkeys.common.service.UserService;
+
+@EzySingleton("userService")
+public class UserServiceImpl implements UserService {
+}
