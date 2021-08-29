98f7eaea08d2bc429a5b36747a1490792fdaa680
vu-luong <vubinhcht@gmail.com>
2021-08-29 23:41:08 +1000

    EzyRoulette 006: Handle login request

--- server/EzyRoulette-plugin/src/main/java/org/youngmonkeys/plugin/controller/UserLoginController.java
+++ server/EzyRoulette-plugin/src/main/java/org/youngmonkeys/plugin/controller/UserLoginController.java
@@ -2,6 +2,11 @@ package org.youngmonkeys.plugin.controller;
 
 import static com.tvd12.ezyfoxserver.constant.EzyEventNames.USER_LOGIN;
 
+import com.tvd12.ezyfox.sercurity.EzySHA256;
+import com.tvd12.ezyfoxserver.constant.EzyLoginError;
+import com.tvd12.ezyfoxserver.exception.EzyLoginErrorException;
+import org.youngmonkeys.common.entity.User;
+import org.youngmonkeys.common.service.UserService;
 import org.youngmonkeys.plugin.service.WelcomeService;
 import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
 import com.tvd12.ezyfox.bean.annotation.EzySingleton;
@@ -17,10 +22,32 @@ public class UserLoginController extends EzyAbstractPluginEventController<EzyUse
 	@EzyAutoBind
 	private WelcomeService welcomeService;
 	
+	@EzyAutoBind
+	private UserService userService;
+	
 	@Override
 	public void handle(EzyPluginContext ctx, EzyUserLoginEvent event) {
 		logger.info("{} login in", welcomeService.welcome(event.getUsername()));
-		logger.info("Username: {}, password {}", event.getUsername(), event.getPassword());
+		
+		String username = event.getUsername();
+		String password = encodePassword(event.getPassword());
+		
+		User user = userService.getUser(username);
+		
+		if (user == null) { // user doesn't exist in db
+			logger.info("user doesn't exist in db, create a new one!");
+			user = userService.createUser(username, password);
+		}
+		
+		if (!user.getPassword().equals(password)) {
+			throw new EzyLoginErrorException(EzyLoginError.INVALID_PASSWORD);
+		}
+		
+		logger.info("user and password match, accept user {}", username);
+	}
+	
+	private String encodePassword(String password) {
+		return EzySHA256.cryptUtfToLowercase(password);
 	}
 	
 }
