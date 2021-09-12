2ead7d78fa7b50f56160ff3d052716e47e80766e
vu-luong <vubinhcht@gmail.com>
2021-09-13 00:14:32 +1000

    EzyRoulette 012: Handle spin response in Unity

--- unity-client/Assets/2 - Scripts/controllers/GameController.cs
+++ unity-client/Assets/2 - Scripts/controllers/GameController.cs
@@ -1,9 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+	public RotateWheel rotateWheel;
+	
+	private void Awake()
+	{
+		SpinResponseHandler.spinResponseEvent += OnSpinResponse;
+	}
+	private void OnSpinResponse(int result)
+	{
+		rotateWheel.Activate(result);
+	}
+
 	public void Spin()
 	{
 		SocketRequest.GetInstance().sendSpinRequest();
